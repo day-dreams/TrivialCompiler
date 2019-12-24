0c3e519c3110c43a0c55572919d8c5c5739d98e8
day-dreams <xyz.zhangnan@gmail.com>
2019-12-25 00:14:48 +0800

    doc: 添加实现步骤说明

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 整个编辑器玩玩。
+
+* [参考](https://www.freecodecamp.org/news/write-a-compiler-in-go-quick-guide-30d2f33ac6e0/)
+
+* [gocc](https://github.com/goccmack/gocc)
+
+* 基本思路
+    1. 定义语法，形成bnf文件
+    2. 用`gocc`生成lexer和parser
+    3. 定义AST
+    4. 借助lexer和parse，实现从源代码到AST的转换
+    5. *可选项* 实现一个类型检查器，检查源代码转换成的AST是否合法
+    6. 实现一个代码生成器，根据AST生成目标代码
+    7. 去执行目标代码
---
+++ test/parser_test.go
@@ -0,0 +1,31 @@
+package test
+
+import (
+	"github.com/day-dreams/TrivialCompiler/detail"
+	"testing"
+)
+
+func TestParser(t *testing.T) {
+	const input = `
+			/* comment should not be scanned */
+			func add() Int {
+				return x + y;
+			}
+
+			let five = "test";
+			let ten = 10;
+
+			let result = 4;  
+			5 < 10;
+
+			if (5 < 10) {
+				print(10);
+			} else {
+				print(5);
+			}
+
+			10 == 10;
+			`
+
+	detail.Parse(input)
+}
